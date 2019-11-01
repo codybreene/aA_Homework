3d33701e3ba0bb72b6c0de4c30a5e5079b15e2da
codybreene <codybreene@gmail.com>
2019-10-31 23:05:49 -0700

    Write initial map class

--- W3D5/diy_adts.rb
+++ W3D5/diy_adts.rb
@@ -50,4 +50,38 @@ class Queue
     end
 end
 
+class Map
+
+    def initialize
+        @imap = []
+    end
+
+    def set(key, value)
+        dupe = false
+        @imap.each_with_index do |char, i|
+            if @imap[i][0] == key
+                dupe = true
+                @imap[i][1] == value
+            end
+        end
+        if dupe == false
+            @imap << [key, value] 
+        end      
+    end
+
+    def get(key)
+        @imap.each {|pair| return pair[1] if pair[0] == key}
+    end
+
+    def delete(key)
+        @imap.with_index do |i|
+            @imap.delete_at(i) if @imap[i].include?(key)
+        end
+    end
+
+    def show
+        @imap
+    end
+end
+
 
