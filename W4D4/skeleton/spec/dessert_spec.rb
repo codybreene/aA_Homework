513d36b215915d2b403b3520046172961cd0c0ce
codybreene <codybreene@gmail.com>
2019-11-06 23:04:00 -0800

    Add initial 5 specs for RSpec exercise

---
+++ W4D4/skeleton/spec/dessert_spec.rb
@@ -0,0 +1,54 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  subject(:dessert) { Dessert.new("brownie", 50, "Cody") }
+  let(:chef) { double("chef") }
+
+  describe "#initialize" do
+    it "sets a type" do 
+      expect(dessert.type).to eq("brownie")
+    end
+
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(50)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to be_empty
+    end
+
+    it "raises an argument error when given a non-integer quantity" do 
+      expect{Dessert.new("brownie", "50", "Cody")}.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do 
+      dessert.add_ingredient("SUGAR")
+      expect(dessert.ingredients).to include("SUGAR")
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array"
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity"
+
+    it "raises an error if the amount is greater than the quantity"
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name"
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in"
+  end
+end
